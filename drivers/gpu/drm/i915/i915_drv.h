ipts-linux-org/ipts-linux-new

drm/i915/gen9: support Intel Precise Touch & Stylus
This patch enables Intel Precise Touch & Stylus(IPTS) support.

IPTS requires GuC to support PCH as its client. So this patch enables GuC submission
and PCH as GuC client.

- enable GuC submission for IPTS
- redirect GT_RENDER_PIPECTL_NOTIFY_INTERRUPT to host and use it to detect a submission completed
- introduce interface to IPTS support

Signed-off-by: Hyungwoo Yang <hyungwoo.yang@intel.com>

drivers/gpu/drm/i915/i915_drv.h
@@ -3402,6 +3402,8 @@ struct drm_i915_gem_object *
 i915_gem_alloc_context_obj(struct drm_device *dev, size_t size);
 struct i915_gem_context *
 i915_gem_context_create_gvt(struct drm_device *dev);
+struct i915_gem_context *
+i915_gem_context_create_ipts(struct drm_device *dev);
 
 static inline struct i915_gem_context *
 i915_gem_context_lookup(struct drm_i915_file_private *file_priv, u32 id)

drivers/gpu/drm/i915/i915_gem_context.c
@@ -405,6 +405,17 @@ out:
 	return ctx;
 }
 
+struct i915_gem_context *i915_gem_context_create_ipts(struct drm_device *dev)
+{
+	struct i915_gem_context *ctx;
+
+	BUG_ON(!mutex_is_locked(&dev->struct_mutex));
+
+	ctx = i915_gem_create_context(dev, NULL);
+
+	return ctx;
+}
+
 static void i915_gem_context_unpin(struct i915_gem_context *ctx,
 				   struct intel_engine_cs *engine)
 {

jimdigriz/linux

drivers/gpu/drm/i915/i915_drv.h
@@ -3406,6 +3406,9 @@ void i915_gem_object_do_bit_17_swizzle(struct drm_i915_gem_object *obj,
 void i915_gem_object_save_bit_17_swizzle(struct drm_i915_gem_object *obj,
 					 struct sg_table *pages);
 
+struct i915_gem_context *
+i915_gem_context_create_ipts(struct drm_i915_private *dev_priv);
+
 static inline struct i915_gem_context *
 i915_gem_context_lookup(struct drm_i915_file_private *file_priv, u32 id)
 {

drivers/gpu/drm/i915/i915_gem_context.c
@@ -407,6 +407,17 @@ out:
 	return ctx;
 }
 
+struct i915_gem_context *i915_gem_context_create_ipts(struct drm_i915_private *dev_priv)
+{
+	struct i915_gem_context *ctx;
+
+	BUG_ON(!mutex_is_locked(&dev->struct_mutex));
+
+	ctx = i915_gem_create_context(dev_priv, NULL);
+
+	return ctx;
+}
+
 int i915_gem_context_init(struct drm_i915_private *dev_priv)
 {
 	struct i915_gem_context *ctx;